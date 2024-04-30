195991d2ed45d0428a2d18ee335b04f4768146b4
Sourabhsoni44 <saurabhsoni252002@gmail.com>
2024-04-30 16:53:01 +0530

    new image

--- app/Event/page.js
+++ app/Event/page.js
@@ -99,7 +99,6 @@ const page = () => {
               src="/_next/image?url=%2Fimages%2Fimg_image_131_105x105.png&w=256&q=75"
             />
           </div>
-
           <label class="inline-flex items-center cursor-pointer">
             <input type="checkbox" value="" class="sr-only peer" />
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
